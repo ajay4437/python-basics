f430238700c960b7ce92e9c5430dc763d1379bf5
ajay4437 <90853550+ajay4437@users.noreply.github.com>
2021-09-16 21:30:00 +0530

    Update logical_operator.py

--- logical_operator.py
+++ logical_operator.py
@@ -1,8 +1,8 @@
-#age=int(input('enter your age: '))
-#if(age>=18 and age<=26):
-#    print('yes')
-#else:
-#    print('no')
+age=int(input('enter your age: '))
+if(age>=18 and age<=26):
+    print('yes')
+else:
+    print('no')
 
 age=int(input('enter your age: '))
 if(age>=18 or age<=26):
